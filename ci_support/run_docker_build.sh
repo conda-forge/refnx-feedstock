bbf1f22c3afc46b31e9556c5b5d8489747416bbb
Andrew Nelson <andyfaff@gmail.com>
2017-08-23 16:39:27 +1000

    MAINT remove all py34 linux

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -91,12 +91,12 @@ source run_conda_forge_build_setup
 #    conda build /recipe_root --quiet || exit 1
 #    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
-    set -x
-    export CONDA_NPY=113
-    export CONDA_PY=34
-    set +x
-    conda build /recipe_root --quiet || exit 1
-    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
+#    set -x
+#    export CONDA_NPY=113
+#    export CONDA_PY=34
+#    set +x
+#    conda build /recipe_root --quiet || exit 1
+#    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
     set -x
     export CONDA_NPY=110
