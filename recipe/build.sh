b1db5e34e8a6cf8ac6e00d6d7e972333840e7c84
Andrew Nelson <andyfaff@gmail.com>
2026-03-18 12:06:53 +1100

    BLD command

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,10 +10,18 @@ fi
 if [[ $build_platform != $target_platform ]]; then
     # write to separate cross-file to not interfere with default cross-python activation, c.f.
     # https://github.com/conda-forge/cross-python-feedstock/blob/91d3c9cf/recipe/activate-cross-python.sh#L111-L125
-    echo "[binaries]"                                   > $SRC_DIR/refnx_cross_file.txt
-    echo "cc=arm64-apple-darwin20.0.0-clang"            > $SRC_DIR/refnx_cross_file.txt
-    echo "cc=arm64-apple-darwin20.0.0-clang++"           > $SRC_DIR/refnx_cross_file.txt
+    echo "[binaries]"                                   >> $SRC_DIR/refnx_cross_file.txt
+    echo "cc=arm64-apple-darwin20.0.0-clang"            >> $SRC_DIR/refnx_cross_file.txt
+    echo "cpp=arm64-apple-darwin20.0.0-clang++"         >> $SRC_DIR/refnx_cross_file.txt
 
+    echo "[host_machine]"                               >> $SRC_DIR/refnx_cross_file.txt
+    echo "cpu=arm64"                                    >> $SRC_DIR/refnx_cross_file.txt
+    echo "system='darwin'"                              >> $SRC_DIR/refnx_cross_file.txt
+    echo "cpu_family='arm'"                             >> $SRC_DIR/refnx_cross_file.txt
+
+    echo "[properties]"                                 >> $SRC_DIR/refnx_cross_file.txt
+    echo "c_args = ['-target', 'arm64-apple-darwin']"   >> $SRC_DIR/refnx_cross_file.txt
+    echo "cpp_args = ['-target', 'arm64-apple-darwin']" >> $SRC_DIR/refnx_cross_file.txt
     export MESON_ARGS="$MESON_ARGS --cross-file=$SRC_DIR/refnx_cross_file.txt"
     $PYTHON -m pip uninstall ninja
 fi
