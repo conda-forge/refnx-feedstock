d35cb0f4c2b72cd6552764a7d39b7b5185e477f3
Andrew Nelson <andyfaff@gmail.com>
2026-03-18 11:35:26 +1100

    BLD command

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,10 +7,17 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     export LDFLAGS="$LDFLAGS -lomp"
 fi
 
+if [[ $build_platform != $target_platform ]]; then
+    # write to separate cross-file to not interfere with default cross-python activation, c.f.
+    # https://github.com/conda-forge/cross-python-feedstock/blob/91d3c9cf/recipe/activate-cross-python.sh#L111-L125
+    echo "[binaries]"                                   > $SRC_DIR/refnx_cross_file.txt
+    export MESON_ARGS="$MESON_ARGS --cross-file=$SRC_DIR/refnx_cross_file.txt"
+fi
+
 mkdir builddir
 
 # -wnx flags mean: --wheel --no-isolation --skip-dependency-check
-$PYTHON -m build -w -n -x \
+$PYTHON -m build -w -n -v -x \
     -Cbuilddir=builddir \
     -Csetup-args=${MESON_ARGS// / -Csetup-args=} \
     || (cat builddir/meson-logs/meson-log.txt && exit 1)
